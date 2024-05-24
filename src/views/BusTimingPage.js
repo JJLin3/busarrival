570693082d1a1df466c42ec025decbb49a452ccc
linjunjie <linjunjie.1995@gmail.com>
2024-05-24 13:50:20 +0800

    Add the functioning bus timing page for now

--- src/views/BusTimingPage.js
+++ src/views/BusTimingPage.js
@@ -1,35 +1,61 @@
 import React from 'react';
 import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import { DataTable } from 'react-native-paper';
+import { config } from '../services/LTAServices';
+import axios from 'axios';
 
-export default function BusTimingPage() {
+export default function BusTimingPage({route}) {
 
-  const [items] = React.useState([
-    {
-      key: 1,
-      busNumber: '883M',
-      arrivalTime: 10,
-      nextArrivalTime: 12,
-    },
-    {
-      key: 2,
-      busNumber: '883',
-      arrivalTime: 3,
-      nextArrivalTime: 10,
-    },
-    {
-      key: 3,
-      busNumber: '169',
-      arrivalTime: 1,
-      nextArrivalTime: 5,
-    },
-    {
-      key: 4,
-      busNumber: '117',
-      arrivalTime: 2,
-      nextArrivalTime: 6,
-    },
-  ])
+  const [ busTiming, setBusTiming ] = React.useState([]);
+  const { busStopCode } = route.params;
+
+  function compareTime(timing){
+    const time1 = new Date();
+    const time2 = new Date(timing);
+    
+    var timeDiff = Math.abs(time2.getMinutes() - time1.getMinutes());
+    // console.log('the minutes: ' +timing)
+    // console.log('the minutes: ' +time2.getMinutes() + " _ " + time1.getMinutes() + " _ " + timeDiff)
+    return(timeDiff)
+  }
+
+  async function getBusArrivalTime(busStop) {
+    console.log('start')
+  let firstInterval = 0;
+  let secondInterval = 0;
+  let thirdInterval = 0;
+  
+  api = `http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2?BusStopCode=${busStop}`;
+  
+  const res = await axios.get(api, config)
+  //.then(response => console.log(response.data))
+  const test = new Date(res.data.Services[0].NextBus.EstimatedArrival)
+  
+  //console.log(Object.keys(res.data.Services[0]).filter((item, index) => item === ))
+  //for(let i = 0; i < 3; i ++){}
+  return(res.data.Services)
+  
+  }
+
+  async function loadAllBusTiming(){
+    let data = await getBusArrivalTime(busStopCode);
+    let newData = [];
+    console.log(JSON.stringify(newData))
+    newData = data.map((busstop) => {
+      let obj = {};
+      obj.ServiceNo = busstop.ServiceNo;
+      obj.arrivalTime = compareTime(busstop.NextBus.EstimatedArrival);
+      obj.nextArrivalTime = compareTime(busstop.NextBus2.EstimatedArrival);
+      return(obj);
+    });
+
+
+    setBusTiming(newData);
+  }
+
+  React.useEffect(()=>{
+    loadAllBusTiming(busStopCode)
+  },[])
 
   return (
     <ScrollView>
@@ -40,9 +66,9 @@ export default function BusTimingPage() {
             <DataTable.Title numeric>Next Arrival Time(mins)</DataTable.Title>
           </DataTable.Header>
 
-          {items.map(item => (
-            <DataTable.Row key={item.key}>
-              <DataTable.Cell>{item.busNumber}</DataTable.Cell>
+          {busTiming.map((item,index) => (
+            <DataTable.Row key={index}>
+              <DataTable.Cell>{item.ServiceNo}</DataTable.Cell>
               <DataTable.Cell numeric>{item.arrivalTime}</DataTable.Cell>
               <DataTable.Cell numeric>{item.nextArrivalTime}</DataTable.Cell>
             </DataTable.Row>
